a154b9f7b76eb32e7ab384ee50e40035fef218e9
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-04-06 18:31:29 +0300

    end v.1.0

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -10,7 +10,11 @@ import java.util.HashMap;
 public class Main {
     public static void main(String[] args) {
         HashMap<Integer,Task> listTask = MemoryTask.ReadTaskList();
-        ConsolViev.run(listTask);
 
+        System.out.println("Добро пожаловать в TaskManager!");
+        System.out.println("У вас в работе "+ listTask.size() +" задач.");
+        System.out.println("Введите help что бы отобразить доступные команды.");
+
+        ConsolViev.run(listTask);
     }
 }
