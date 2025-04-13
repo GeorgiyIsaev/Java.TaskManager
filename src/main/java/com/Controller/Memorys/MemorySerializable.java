8d9edebddbc6ccb0fe4cda1745b6890c55084733
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-04-13 13:54:17 +0300

    memory

---
+++ src/main/java/com/Controller/Memorys/MemorySerializable.java
@@ -0,0 +1,13 @@
+package com.Controller.Memorys;
+
+public class MemorySerializable extends Memory{
+    @Override
+    public void load() {
+        super.load();
+    }
+
+    @Override
+    public void save() {
+        super.save();
+    }
+}
---
+++ src/main/java/com/Controller/Memorys/MemoryTXT.java
@@ -0,0 +1,14 @@
+package com.Controller.Memorys;
+
+public class MemoryTXT extends Memory{
+
+    @Override
+    public void load() {
+        super.load();
+    }
+
+    @Override
+    public void save() {
+        super.save();
+    }
+}
