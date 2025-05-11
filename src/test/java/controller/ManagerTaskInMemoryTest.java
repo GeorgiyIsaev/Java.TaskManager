4b36b071ca2f61301e35b0c87c878f0cc3348d2b
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-05-12 01:22:36 +0300

    [junit test] Checking class ManagerTaskInMemoryTest the task life cycle (creation, modification and deletion) has been checked

--- src/test/java/controller/ManagerTaskInMemoryTest.java
+++ src/test/java/controller/ManagerTaskInMemoryTest.java
@@ -2,10 +2,15 @@ package controller;
 
 import com.controller.IManagerTask;
 import com.controller.Managers;
+import com.controller.controlException.NotExistIdException;
+import com.dateTask.SubTask;
 import com.dateTask.Task;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import com.dateTask.TaskStatus;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Objects;
 
 public class ManagerTaskInMemoryTest {
 
@@ -41,14 +46,79 @@ public class ManagerTaskInMemoryTest {
 
     @Test
     public void addSubTask(){
+        Task epic = managerTask.addEpic("EPIC", "description EPIC");
         String name = "Тестовое название SubTask";
         String description = "Тестовое описание SubTask";
         int oldSize = managerTask.getTaskMap().size();
-        Task task = managerTask.addEpic(name, description);
+        Task task = managerTask.addSubTaskToEpicID(epic.getID(), name, description);
         int newSize = managerTask.getTaskMap().size();
         Assertions.assertTrue(newSize>oldSize);
-        Assertions.assertTrue("EPIC".equalsIgnoreCase(task.getTypeTask()));
+        Assertions.assertTrue("SubTask".equalsIgnoreCase(task.getTypeTask()));
+        boolean isIdExists = epic.findID(task.getID());
+        Assertions.assertTrue(isIdExists);
         System.out.println("Добавлен " + task.getTypeTask() + ": ID " +task.getID()+ " - " + task);
+    }
+
+    @Nested
+    class TaskLifeCycle{
+        static int idTask;
+        @BeforeAll
+        public static void addTask(){
+            String name = "Тестовое название жиз";
+            String description = "Тестовое описание жиз";
+            int oldSize = managerTask.getTaskMap().size();
+            Task task = managerTask.addTask(name, description);
+            idTask = task.getID();
+            int newSize = managerTask.getTaskMap().size();
+            Assertions.assertTrue(newSize>oldSize);
+            Assertions.assertTrue("TASK".equalsIgnoreCase(task.getTypeTask()));
+            System.out.println("Добавлен " + task.getTypeTask() + ": ID " +task.getID()+ " - " + task);
+        }
+        @Test
+        public void reNameTask(){
+            Task task = managerTask.getTask(idTask);
+            System.out.println("Переименовывается " + task.getTypeTask() + ": ID " +task.getID()+ " - " + task);
+            String newName = "Новое название";
+            managerTask.reNameToIDTask(idTask, newName);
+            Assertions.assertTrue(newName.equalsIgnoreCase(task.getName()));
+            System.out.println("Изменение " + task.getTypeTask() + ": ID " +task.getID()+ " - " + task);
+        }
+
+        @Test
+        public void reDescTask(){
+            Task task = managerTask.getTask(idTask);
+            System.out.println("Переименовывается " + task.getTypeTask() + ": ID " +task.getID()+ " - " + task);
+            String newName = "Новое описание";
+            managerTask.reDescToIDTask(idTask, newName);
+            Assertions.assertTrue(newName.equalsIgnoreCase(task.getDescription()));
+            System.out.println("Изменение " + task.getTypeTask() + ": ID " +task.getID()+ " - " + task);
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = { "NEW", "IN_PROGRESS", "DONE", "NEW" })
+        public void reStatusTask(TaskStatus taskStatus){
+            Task task = managerTask.getTask(idTask);
+            managerTask.reStatus(idTask, taskStatus);
+            Assertions.assertEquals(taskStatus, task.getTaskStatus());
+            System.out.println("Статус " + task.getTypeTask() + ": ID " +task.getID()+ " - " + task
+                + " --> " + task.getTaskStatus());
+
+        }
+
+
+
+        @AfterAll
+        public static void removeTask(){
+            Task task = managerTask.getTask(idTask);
+            System.out.println("Удаляется " + task.getTypeTask() + ": ID " +task.getID()+ " - " + task);
+
+            managerTask.deleteIDTask(idTask);
+            Assertions.assertThrows(NotExistIdException.class, ()->{
+                Task taskBeforeDeleteTask =  managerTask.getTask(idTask);
+            });
+            System.out.println("Task c id " + idTask + " удален!");
+        }
+
     }
 
 
