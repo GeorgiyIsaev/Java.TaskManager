5a03ad70f168656dab2dcb2d0d45b840c58e2dfb
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-05-13 03:10:51 +0300

    [junit test] Checking the existence of a sub task. Creating, changing, deleting

--- src/test/java/dataTask/SubTaskTest.java
+++ src/test/java/dataTask/SubTaskTest.java
@@ -1,4 +1,60 @@
 package dataTask;
 
+import com.dateTask.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 public class SubTaskTest {
+    public static EpicTask epicTask;
+    public static SubTask subTask;
+
+    @BeforeAll
+    public static void createTask(){
+        epicTask = new EpicTask(CreateID.INSTANCE.createID(),
+                "Эпик для теста","Описание Эпика");
+
+
+        int id = CreateID.INSTANCE.createID();
+        String name = "Название Саб";
+        String description = "Описание Саб";
+        subTask = new SubTask(id,name,description, epicTask);
+
+        Assertions.assertEquals(name, subTask.getName());
+        Assertions.assertEquals(description, subTask.getDescription());
+        Assertions.assertEquals(id, subTask.getID());
+        Assertions.assertEquals(TaskStatus.NEW, subTask.getTaskStatus());
+        Assertions.assertEquals(CONST.SUB_NAME, subTask.getTypeTask());
+        Assertions.assertEquals("" + epicTask.getID(), subTask.getLinkStr());
+
+    }
+
+    @Test
+    public void changeTaskName(){
+        String newName ="Новое имя Саб";
+        subTask.setName(newName);
+        Assertions.assertEquals(newName, subTask.getName());
+    }
+    @Test
+    public void changeTaskDescription(){
+        String newDesc ="Новое описание Саб";
+        subTask.setDescription(newDesc);
+        Assertions.assertEquals(newDesc, subTask.getDescription());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "NEW", "IN_PROGRESS", "DONE", "NEW" })
+    public void changeTaskStatus(TaskStatus taskStatus){
+        subTask.setTaskStatus(taskStatus);
+        Assertions.assertEquals(taskStatus, subTask.getTaskStatus());
+    }
+
+    @AfterAll
+    public static void deleteSub(){
+        epicTask.deleteSubTask(subTask);
+        Assertions.assertFalse(epicTask.findID(subTask.getID()));
+    }
 }
