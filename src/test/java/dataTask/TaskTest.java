5a52056a59ca75c198d335c74a55ebd5cea010b8
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-06-16 23:10:14 +0300

    [JUnit Test] add class TaskTest

--- src/test/java/dataTask/TaskTest.java
+++ src/test/java/dataTask/TaskTest.java
@@ -1,18 +1,112 @@
 package dataTask;
 
 import com.dateTask.Task;
+import com.dateTask.TaskStatus;
+import com.dateTask.TypeTask;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class TaskTest {
 
 
     @Test
-    public void tes1t(){
+    public void CheckThatTheTaskReturnsTheCorrectType(){
         Task task = new Task("", "");
+        String typeTasK = task.getTypeTask();
+        String typeTasKShouldBy = TypeTask.TASK_NAME;
+        Assertions.assertEquals(typeTasKShouldBy, typeTasK);
     }
 
     @Test
-    public void test(){
-
+    public void TheIDOfNewTaskMustBeOneMoreThanPreviousOne(){
+        Task task1 = new Task("", "");
+        Task task2 = new Task("", "");
+        Assertions.assertEquals(task1.getID() +1 , task2.getID());
     }
+
+    @Test
+    public void  TheReturnedTaskNameShouldMatchTheOnePassedToConstructor(){
+        String nameTask = "Имя тестовой задачи";
+        Task task = new Task(nameTask, "");
+        Assertions.assertEquals(nameTask, task.getName());
+    }
+    @Test
+    public void  TheReturnedTaskDescriptionShouldMatchTheOnePassedToConstructor(){
+        String description = "Описание тестовой задачи";
+        Task task = new Task("", description);
+        Assertions.assertEquals(description, task.getDescription());
+    }
+
+    @Test
+    public void TaskShouldNotHaveAnyConnections(){
+        Task task = new Task("", "");
+        String infoConnections = TypeTask.NO_REFERENCE;
+        Assertions.assertEquals(infoConnections, task.getLinkStr());
+    }
+
+
+    @Test
+    public void TaskStatusShouldBeSetToNewWhenCreated(){
+        Task task = new Task("", "");
+        Assertions.assertEquals(TaskStatus.NEW, task.getStatus());
+    }
+
+
+    @Test
+    public void TaskShouldChangeItsStatusToDONE(){
+        Task task = new Task("", "");
+        task.setStatus(TaskStatus.DONE);
+        Assertions.assertEquals(TaskStatus.DONE, task.getStatus());
+    }
+    @Test
+    public void TaskShouldChangeItsStatusToPROGRESS(){
+        Task task = new Task("", "");
+        task.setStatus(TaskStatus.IN_PROGRESS);
+        Assertions.assertEquals(TaskStatus.IN_PROGRESS, task.getStatus());
+    }
+
+
+    @Test
+    public void TaskShouldChangeTheName(){
+        Task task = new Task("", "");
+        String newName = "Новое имя задачи";
+        task.setName(newName);
+        Assertions.assertEquals(newName, task.getName());
+    }
+
+    @Test
+    public void TaskShouldChangeTheDescription(){
+        Task task = new Task("", "");
+        String newDescription = "Новое описание задачи";
+        task.setDescription(newDescription);
+        Assertions.assertEquals(newDescription, task.getDescription());
+    }
+
+    @Test
+    public void TaskShouldReturnStringDescription(){
+        String name = "Имя";
+        String description = "Описание";
+        Task task = new Task(name, description);
+        String resultIT = name + " {" + description + "}";
+        Assertions.assertEquals(resultIT, task.toString());
+    }
+
+    @Test
+    public void SearchingForAnIdInTaskShouldConfirmThatTaskHasThatId(){
+        Task task = new Task("", "");
+        int id = task.getID();
+        boolean isFind = task.findID(id);
+        Assertions.assertTrue(isFind);
+    }
+
+    @Test
+    public void SearchingForAnIdInTaskShouldNOTConfirmThatTaskHasThatId(){
+        Task task = new Task("", "");
+        int id = task.getID() + 1;
+        boolean isFind = task.findID(id);
+        Assertions.assertFalse(isFind);
+    }
+
+
+
 }
