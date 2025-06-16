74ebc54be3de11ca834f494f28f19adf967ad9c8
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-06-16 23:30:21 +0300

    [JUnit Test] add class EpicTaskTest and SubTaskTest

--- src/test/java/dataTask/EpicTaskTest.java
+++ src/test/java/dataTask/EpicTaskTest.java
@@ -1,4 +1,114 @@
 package dataTask;
 
+import com.dateTask.EpicTask;
+import com.dateTask.Task;
+import com.dateTask.TaskStatus;
+import com.dateTask.TypeTask;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class EpicTaskTest {
+
+
+
+    @Test
+    public void CheckThatEpicReturnsTheCorrectType(){
+        Task task = new EpicTask("", "");
+        String typeTasK = task.getTypeTask();
+        String typeTasKShouldBy = TypeTask.EPIC_NAME;
+        Assertions.assertEquals(typeTasKShouldBy, typeTasK);
+    }
+
+    @Test
+    public void TheIDOfNewEpicMustBeOneMoreThanPreviousOne(){
+        Task task1 = new EpicTask("", "");
+        Task task2 = new EpicTask("", "");
+        Assertions.assertEquals(task1.getID() +1 , task2.getID());
+    }
+
+    @Test
+    public void  TheReturnedEpicNameShouldMatchTheOnePassedToConstructor(){
+        String nameTask = "Имя тестовой задачи";
+        Task task = new EpicTask(nameTask, "");
+        Assertions.assertEquals(nameTask, task.getName());
+    }
+    @Test
+    public void  TheReturnedEpicDescriptionShouldMatchTheOnePassedToConstructor(){
+        String description = "Описание тестовой задачи";
+        Task task = new EpicTask("", description);
+        Assertions.assertEquals(description, task.getDescription());
+    }
+
+    @Test
+    public void EpicShouldNotHaveAnyConnections(){
+        Task task = new EpicTask("", "");
+        String infoConnections = "[]"; // список Sub пуст
+        Assertions.assertEquals(infoConnections, task.getLinkStr(), task.getLinkStr());
+    }
+
+
+    @Test
+    public void EpicStatusShouldBeSetToNewWhenCreated(){
+        Task task = new EpicTask("", "");
+        Assertions.assertEquals(TaskStatus.NEW, task.getStatus());
+    }
+
+
+    @Test
+    public void EpicShouldCNOThangeItsStatusToDONE(){
+        Task task = new EpicTask("", "");
+        TaskStatus statusOld = task.getStatus();
+        task.setStatus(TaskStatus.DONE);
+        Assertions.assertEquals(statusOld, task.getStatus());
+    }
+    @Test
+    public void EpicShouldNOTChangeItsStatusToPROGRESS(){
+        Task task = new EpicTask("", "");
+        TaskStatus statusOld = task.getStatus();
+        task.setStatus(TaskStatus.IN_PROGRESS);
+        Assertions.assertEquals(statusOld, task.getStatus());
+    }
+
+
+    @Test
+    public void EpicShouldChangeTheName(){
+        Task task = new EpicTask("", "");
+        String newName = "Новое имя задачи";
+        task.setName(newName);
+        Assertions.assertEquals(newName, task.getName());
+    }
+
+    @Test
+    public void EpicShouldChangeTheDescription(){
+        Task task = new EpicTask("", "");
+        String newDescription = "Новое описание задачи";
+        task.setDescription(newDescription);
+        Assertions.assertEquals(newDescription, task.getDescription());
+    }
+
+    @Test
+    public void TaskShouldReturnStringDescription(){
+        String name = "Имя";
+        String description = "Описание";
+        Task task = new EpicTask(name, description);
+        String resultIT = name + " {" + description + "}";
+        Assertions.assertEquals(resultIT, task.toString());
+    }
+
+    @Test
+    public void SearchingForAnIdInEpicShouldConfirmThatTaskHasThatId(){
+        Task task = new EpicTask("", "");
+        int id = task.getID();
+        boolean isFind = task.findID(id);
+        Assertions.assertTrue(isFind);
+    }
+
+    @Test
+    public void SearchingForAnIdInEpicShouldNOTConfirmThatTaskHasThatId(){
+        Task task = new EpicTask("", "");
+        int id = task.getID() + 1;
+        boolean isFind = task.findID(id);
+        Assertions.assertFalse(isFind);
+    }
+
 }
---
+++ src/test/java/dataTask/SubTaskTest.java
@@ -0,0 +1,124 @@
+package dataTask;
+
+import com.dateTask.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SubTaskTest {
+
+    EpicTask epic;
+
+    @BeforeEach
+    public void createEpic(){
+        epic = new EpicTask("Эпик", "Описание");
+    }
+
+
+    @Test
+    public void CheckThatTheSubReturnsTheCorrectType(){
+        Task task = new SubTask("", "", epic);
+        String typeTasK = task.getTypeTask();
+        String typeTasKShouldBy = TypeTask.SUB_NAME;
+        Assertions.assertEquals(typeTasKShouldBy, typeTasK);
+    }
+
+    @Test
+    public void TheIDOfNewSubMustBeOneMoreThanPreviousOne(){
+        Task task1 = new SubTask("", "", epic);
+        Task task2 = new SubTask("", "", epic);
+        Assertions.assertEquals(task1.getID() +1 , task2.getID());
+    }
+
+    @Test
+    public void  TheReturnedSubNameShouldMatchTheOnePassedToConstructor(){
+        String nameTask = "Имя тестовой задачи";
+        Task task = new SubTask(nameTask, "", epic);
+        Assertions.assertEquals(nameTask, task.getName());
+    }
+    @Test
+    public void  TheReturnedSubDescriptionShouldMatchTheOnePassedToConstructor(){
+        String description = "Описание тестовой задачи";
+        Task task = new SubTask("", description, epic);
+        Assertions.assertEquals(description, task.getDescription());
+    }
+
+    @Test
+    public void SubShouldNotHaveAnyConnections(){
+        Task task = new SubTask("", "", epic);
+        String infoConnections = epic.getID() + "";
+        Assertions.assertEquals(infoConnections, task.getLinkStr(),  task.getLinkStr());
+    }
+
+
+    @Test
+    public void SubStatusShouldBeSetToNewWhenCreated(){
+        Task task = new SubTask("", "", epic);
+        Assertions.assertEquals(TaskStatus.NEW, task.getStatus());
+    }
+
+
+    @Test
+    public void SubShouldChangeItsStatusToDONE(){
+        Task task = new SubTask("", "", epic);
+        task.setStatus(TaskStatus.DONE);
+        Assertions.assertEquals(TaskStatus.DONE, task.getStatus());
+    }
+    @Test
+    public void SubShouldChangeItsStatusToPROGRESS(){
+        Task task = new SubTask("", "", epic);;
+        task.setStatus(TaskStatus.IN_PROGRESS);
+        Assertions.assertEquals(TaskStatus.IN_PROGRESS, task.getStatus());
+    }
+
+
+    @Test
+    public void SubShouldChangeTheName(){
+        Task task = new SubTask("", "", epic);;
+        String newName = "Новое имя задачи";
+        task.setName(newName);
+        Assertions.assertEquals(newName, task.getName());
+    }
+
+    @Test
+    public void SubShouldChangeTheDescription(){
+        Task task = new SubTask("", "", epic);;
+        String newDescription = "Новое описание задачи";
+        task.setDescription(newDescription);
+        Assertions.assertEquals(newDescription, task.getDescription());
+    }
+
+    @Test
+    public void SubShouldReturnStringDescription(){
+        String name = "Имя";
+        String description = "Описание";
+        Task task = new SubTask(name, description, epic);;
+        String resultIT = name + " {" + description + "}";
+        Assertions.assertEquals(resultIT, task.toString());
+    }
+
+    @Test
+    public void SearchingForAnIdInSubShouldConfirmThatTaskHasThatId(){
+        Task task = new SubTask("", "", epic);;
+        int id = task.getID();
+        boolean isFind = task.findID(id);
+        Assertions.assertTrue(isFind);
+    }
+
+    @Test
+    public void SearchingForAnIdInSubShouldNOTConfirmThatTaskHasThatId(){
+        Task task = new SubTask("", "", epic);;
+        int id = task.getID() + 1;
+        boolean isFind = task.findID(id);
+        Assertions.assertFalse(isFind);
+    }
+
+
+    @Test
+    public void TheLinkToEpicShouldBeVerified(){
+        SubTask task = new SubTask("", "", epic);
+        EpicTask epicTaskRef = task.getRefrains();
+        Assertions.assertEquals(epic, epicTaskRef);
+    }
+
+}
