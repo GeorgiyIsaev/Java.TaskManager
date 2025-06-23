9a0b108c66c376dd7f203d30995eecc08a1da2d8
georgiyelbaf@gmail.com <georgiyelbaf@gmail.com>
2025-06-24 00:19:34 +0300

    [JUnit] Created a test to check if history is written when calling get. Fixed the get method to save changes to history

--- src/main/java/com/controller/taskManager/FileBackedTasksManager.java
+++ src/main/java/com/controller/taskManager/FileBackedTasksManager.java
@@ -13,65 +13,82 @@ public class FileBackedTasksManager extends InMemoryTaskManager{
        this.fileBackedCSV = new FileBackedCSV(fileName);
        this.fileBackedCSV.load(this);
    }
+
    private void save(){
        this.fileBackedCSV.save(this);
    }
 
 
-
+    @Override
     public Task addTask(String name, String description){
        Task task = super.addTask(name, description);
        save();
        return task;
     }
+
+    @Override
     public Task addEpic(String name, String description){
         Task task = super.addEpic(name, description);
         save();
         return task;
     }
+
+    @Override
     public Task addSubTaskToEpicID(int idEpic, String name, String description){
         Task task = super.addSubTaskToEpicID(idEpic, name, description);
         save();
         return task;
     }
 
+    @Override
     public Task addTaskByID(int id, String name, String description){
         Task task = super.addTaskByID(id, name, description);
         save();
         return task;
     }
+
+    @Override
     public Task addEpicByID(int id,String name, String description){
         Task task = super.addEpicByID(id, name, description);
         save();
         return task;
     }
+
+    @Override
     public Task addSubTaskToEpicIDByID(int idSub, int idEpicAdded, String name, String description){
         Task task = super.addSubTaskToEpicIDByID(idSub,idEpicAdded,name, description);
         save();
         return task;
     }
 
-
+    @Override
     public void deleteALL(){
         super.deleteALL();
         save();
     }
+
+    @Override
     public Task deleteIDTask(int idTask){
         Task task = super.deleteIDTask(idTask);
         save();
         return task;
     }
 
+    @Override
     public Task reNameToIDTask(int idTask, String newName){
         Task task = super.reNameToIDTask(idTask,newName);
         save();
         return task;
     }
+
+    @Override
     public Task reDescToIDTask(int idTask, String newDescription){
         Task task = super.reDescToIDTask(idTask, newDescription);
         save();
         return task;
     }
+
+    @Override
     public boolean reStatus(int idTask, TaskStatus taskStatus){
         boolean result =  super.reStatus(idTask,taskStatus);
         save();
@@ -82,5 +99,12 @@ public class FileBackedTasksManager extends InMemoryTaskManager{
     public void replacementTasks(Map<Integer, Task> tasks) {
         super.replacementTasks(tasks);
         save();
+    }
+
+    @Override
+    public Task getTask(int idTask){
+        Task task = super.getTask(idTask);
+        save();
+        return task;
     }
 }
---
+++ src/test/java/controller/fileBacked/FileBackedTasksManagerHistoryTest.java
@@ -0,0 +1,127 @@
+package controller.fileBacked;
+
+import com.controller.Managers;
+import com.controller.taskManager.TaskManager;
+import com.dateTask.Task;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class FileBackedTasksManagerHistoryTest {
+
+    public TaskManager getClearManager(){
+        TaskManager taskManager = Managers.getFileBacked("MyCSVTest.csv");
+        taskManager.deleteALL();
+        return taskManager;
+    }
+
+    public TaskManager getManager(){
+        return Managers.getFileBacked("MyCSVTest.csv");
+    }
+
+    @Test
+    public void whenCreatingManagerTheHistoryShouldBeEmpty(){
+        TaskManager taskManager = getClearManager();
+        int emptyContainer = 0;
+
+        TaskManager taskManagerInFile = getManager();
+        int sizeHistoryContainer =  taskManagerInFile.getHistory().size();
+        Assertions.assertEquals(emptyContainer,sizeHistoryContainer);
+    }
+
+
+    @Test
+    public void whenAddingManagerTheHistoryShouldBeEmpty(){
+        TaskManager taskManager = getClearManager();
+        Task createTask = taskManager.addTask("","");
+        int emptyContainer = 0;
+
+        TaskManager taskManagerInFile = getManager();
+        int sizeHistoryContainer =  taskManagerInFile.getHistory().size();
+        Assertions.assertEquals(emptyContainer,sizeHistoryContainer);
+    }
+
+    @Test
+    public void historyShouldBeWrittenWhenTaskIsCalled(){
+        TaskManager taskManager = getClearManager();
+        Task createTask = taskManager.addTask("","");
+        taskManager.getTask(createTask.getID());
+
+
+        TaskManager taskManagerInFile = getManager();
+        int sizeHistoryContainer =  taskManagerInFile.getHistory().size();
+        int oneElement = 1;
+        Assertions.assertEquals(oneElement,sizeHistoryContainer);
+
+        final int FIRST_ELEMENT = 0;
+        Task taskFromHistory = taskManagerInFile.getHistory().get(FIRST_ELEMENT);
+        Assertions.assertEquals(createTask.getID(),taskFromHistory.getID());
+    }
+
+
+    @Test
+    public void whenCallingSameTasksTheHistoryShouldBeWrittenOnlyOnce(){
+        TaskManager taskManager = getClearManager();
+        Task createTask = taskManager.addTask("","");
+        taskManager.getTask(createTask.getID());
+        taskManager.getTask(createTask.getID());
+
+        int sizeHistoryContainer =  taskManager.getHistory().size();
+        int oneElement = 1;
+        Assertions.assertEquals(oneElement,sizeHistoryContainer);
+
+        TaskManager taskManagerInFile = getManager();
+        Task taskFromHistory = taskManagerInFile.getHistory().get(0);
+        Assertions.assertEquals(createTask.getID(),taskFromHistory.getID());
+    }
+
+
+    @Test
+    public void taskShouldBeRemovedFromHistoryWhenTaskIsDeleted(){
+        TaskManager taskManager = getClearManager();
+        Task createTask = taskManager.addTask("","");
+        taskManager.getTask(createTask.getID());
+        Task taskFromHistory = taskManager.getHistory().get(0);
+        Assertions.assertEquals(createTask,taskFromHistory);
+
+        taskManager.deleteIDTask(createTask.getID());
+        int emptyContainer = 0;
+
+        TaskManager taskManagerInFile = getManager();
+        int sizeHistoryContainer =  taskManagerInFile.getHistory().size();
+        Assertions.assertEquals(emptyContainer,sizeHistoryContainer);
+    }
+
+    @Test
+    public void onlyLastTenCallsShouldBeRecordedInHistory(){
+        TaskManager taskManager = getClearManager();
+
+        Task createOneTask = taskManager.addTask("","");
+        taskManager.getTask(createOneTask.getID());
+        for(int i= 0 ; i<15; i++){
+            Task tempTask = taskManager.addTask("","");
+            taskManager.getTask(tempTask.getID());
+        }
+        Task createLastTask = taskManager.addTask("","");
+        taskManager.getTask(createLastTask.getID());
+
+        int sizeHistoryContainer =  taskManager.getHistory().size();
+        int maxSizeContainer = 10;
+        Assertions.assertEquals(maxSizeContainer,sizeHistoryContainer);
+
+        TaskManager taskManagerInFile = getManager();
+        Assertions.assertFalse(isInContainer(createOneTask, taskManagerInFile.getHistory()));
+        Assertions.assertTrue(isInContainer(createLastTask, taskManagerInFile.getHistory()));
+    }
+
+    public boolean isInContainer(Task findTask, List<Task> tasks){
+        for (Task task : tasks) {
+            if(task.getID() == findTask.getID()){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
